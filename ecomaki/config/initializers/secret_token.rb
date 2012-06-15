4fbbdd4344ada4c742a9950a7128f7f86a58a1c0
yut <>
2012-06-15 22:03:56 +0900

    rails new ecomaki.

---
+++ ecomaki/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecomaki::Application.config.secret_token = '7acaf6118312e1c941ac507c59bb3b2c0c424ba7c7e9c5f5d8642fc90e973d9ebff1bbf9b70533842a862d690fb668d22bf5311249636a19f05864d1e72c14f6'
