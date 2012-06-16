7025ace850108f31b971e95adcae8e48c99aebfa
yut <>
2012-06-17 01:54:10 +0900

    Set anonymouse user as seed.

--- ecomaki/db/seeds.rb
+++ ecomaki/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Author.create do |author|
+  author.id = 0
+  author.name = "Anonymous"
+  author.password = "NoPassword"
+end
