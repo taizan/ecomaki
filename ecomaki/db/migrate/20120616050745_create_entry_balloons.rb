328bb253cecffef5f44580b8f18a655f666ff196
yut <>
2012-06-16 15:35:25 +0900

    Minor fix: forgotten entry_id in entry_balloons.

--- ecomaki/db/migrate/20120616050745_create_entry_balloons.rb
+++ ecomaki/db/migrate/20120616050745_create_entry_balloons.rb
@@ -1,6 +1,7 @@
 class CreateEntryBalloons < ActiveRecord::Migration
   def change
     create_table :entry_balloons do |t|
+      t.integer :entry_id
       t.string :content
       t.integer :x
       t.integer :y
