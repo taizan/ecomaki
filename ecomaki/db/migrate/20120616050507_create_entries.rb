d1a8dfb6e1a3171b20091f558a060d1684318665
yut <>
2012-06-17 19:00:52 +0900

    Change the column name.

--- ecomaki/db/migrate/20120616050507_create_entries.rb
+++ ecomaki/db/migrate/20120616050507_create_entries.rb
@@ -2,10 +2,10 @@ class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
       t.integer :parent_entry_id
-      t.integer :author
+      t.integer :author_id
       t.integer :width
       t.integer :height
-      t.string :type
+      t.string :create_type
 
       t.timestamps
     end
