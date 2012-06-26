582a16bb4b8cd641d14d5e85c0517a0f85497012
yut <>
2012-06-26 14:57:51 +0900

    Implement index method for ChaptersController.

--- ecomaki/app/controllers/chapters_controller.rb
+++ ecomaki/app/controllers/chapters_controller.rb
@@ -1,4 +1,11 @@
 class ChaptersController < ApplicationController
+  def index
+    novel_id = params[:novel_id]
+    chapters = Chapter.where("novel_id = ?", novel_id)
+    respond_to do |format|
+      format.xml { render :xml => chapters }
+    end
+  end
   def show
     novel_id = params[:novel_id]
     id = params[:id]
