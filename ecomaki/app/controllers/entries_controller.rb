d2b0bce87737d9d61af347104251971807bbdfee
yut <>
2012-07-06 14:19:17 +0900

    Add a controller for Entries.

---
+++ ecomaki/app/controllers/entries_controller.rb
@@ -0,0 +1,9 @@
+
+class EntriesController < ApplicationController
+  def update
+    novel_id = params[:novel_id]
+    id = params[:id]
+    balloons = params[:balloons]
+    characters = params[:characters]
+  end
+end
