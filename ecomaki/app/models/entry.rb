283306a58928bae67571a418806985c38b293abe
yut <>
2012-08-26 21:23:45 +0900

    Minor changes.

--- ecomaki/app/models/entry.rb
+++ ecomaki/app/models/entry.rb
@@ -2,8 +2,9 @@ class Entry < ActiveRecord::Base
   attr_accessible :author_id, :height, :novel_id, :parent_entry_id, :create_type, :width
 
 
-  belongs_to :chapter_entry
+  has_one :chapter_entry
   has_one :chapter, :through => :chapter_entry
+
   belongs_to :author
 
   has_many :entry_character
