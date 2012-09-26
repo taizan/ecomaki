a98f75388119c778f8daaf785fbfba839070dd6b
Taizan <taizan@example.com>
2012-09-27 03:25:23 +0900

    editable title and description

--- ecomaki/app/assets/javascripts/textEdit.js
+++ ecomaki/app/assets/javascripts/textEdit.js
@@ -1,15 +1,21 @@
 
 
 function editableTextarea(target,callback){
-                var text = $('.text',target).html().split("<br>").join('\n');
+                var text = $('.text',target).html();
+		if(text == null || text == undefined){ 
+			text = "[        ]";
+		}else{
+		console.log(text);
+		text = text.split("<br>").join('\n');
                 text = text
                         .replace(/&amp;/g,"&")
                         .replace(/&quot;/g,"/")
                         .replace(/&#039;/g,"'")
                         .replace(/&lt;/g,"<")
                         .replace(/&gt;/g,">");
+		}
 
-                focusedText = $( '<textarea style="text-align:center;" ></textarea>' )
+                var focusedText = $( '<textarea style="text-align:center;" ></textarea>' )
                         .height( $(target).height() ).width ( $(target).width() )
                         .css({position: 'absolute', left:-5 ,top: -5})
                         .appendTo(target)
