1a983f10947ced65ead52624b664345109d7ef78
hitode909 <>
2012-09-30 13:32:58 +0900

    making bgm

--- app/assets/javascripts/config.js
+++ app/assets/javascripts/config.js
@@ -25,3 +25,9 @@ Config.prototype.background_urltoid =  function(url) {
         return null;
     return elements[elements.length-1];
 }
+
+Config.prototype.musics = ['dream', 'flower', 'orange'];
+
+Config.prototype.music_id_to_url = function(music_id) {
+    return "https://dl.dropbox.com/u/8270034/sketch/bgm/" + Config.prototype.musics[music_id] + ".mp3";
+}
