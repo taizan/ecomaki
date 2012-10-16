34a705b4f0de9de579eb7771ecb89ed95bd4a83a
Taizan <taizan@example.com>
2012-10-16 23:48:42 +0900

    use toolMenu.js from wPaint so We can add other menu items without changing wPint plan to use for animation menu

--- app/assets/javascripts/tools/toolMenu.js
+++ app/assets/javascripts/tools/toolMenu.js
@@ -1,1 +1,27 @@
-tootMenu = function();
+$(function(){
+  toolMenu = new ToolMenu();
+  toolMenu.init();
+});
+
+
+function ToolMenu(){}
+
+
+ToolMenu.prototype = {
+  menu : null,
+
+  init : function(){
+	  var menuHandle = $('<div class="_tool_handle"></div>');
+
+    ToolMenu.prototype.menu =  
+        $('<div class="_tool_menu"></div>')
+          .css({position: 'fixed', left:  5, top:  5})
+          .draggable({
+              handle: menuHandle,
+            })
+          .append(menuHandle)
+      
+    $('body').append(ToolMenu.prototype.menu);
+  }
+
+}
