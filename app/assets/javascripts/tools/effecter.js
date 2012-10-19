bbfb71a282d8a670d99fbd4ae756cc63617a1762
ty <ty@t-ys-MacBook-Air.local>
2012-10-19 09:02:20 +0900

    mv effect functions to effecter.js for use other part such as canvas

---
+++ app/assets/javascripts/tools/effecter.js
@@ -0,0 +1,130 @@
+function Effecter(target,item,key){
+	this.target = target;
+	this.item = item;
+	this.key = key;
+	 _.bindAll(this,
+        "changeSelecter",
+        "runSelectedEffect",
+        "onPreDisplay",
+        "runEffect",
+        "effectCallback",
+        "resetEffect",
+        "appendEffectSelecterTo",
+        "setEffect"
+      );
+};
+
+Effecter.prototype = {
+	
+  changeSeleccter: function(){
+    console.log('on item click');
+    $('._tool_menu .effect_selecter').remove();
+    this.appendEffectSelecterTo($('._tool_menu'));
+  },
+	
+	runSelectedEffect: function(){
+		var option = this.item.get(key);
+    if(option != null){
+      var options = option.split(',');
+      this.runEffect(options[0],options[1],parseInt(options[2]),{});
+    }
+    //this.runEffect(this.model.get('option'));
+	},
+	
+  isRunnable: true,
+
+  runEffect: function(selectedFunction , selectedEffect , speed , options){
+    var options = {};
+    // some effects have required parameters
+    if ( selectedEffect === "scale" ) {
+      options = { percent: 0 };
+    } else if ( selectedEffect === "size" ) {
+      options = { to: { width: 200, height: 60 } };
+    } 
+    //prevent overrapping of effect    
+    if(this.isRunnable){
+      switch(selectedFunction){
+        case "show" :
+          if(selectedEffect == 'none'){
+            $(this.target).show( speed, this.effectCallback );
+          }else{
+            $(this.target).show( selectedEffect, options, speed, this.effectCallback );
+          }
+          break; 
+        case "hide" :
+          if(selectedEffect == 'none'){
+            $(this.target).hide( speed, this.effectCallback );
+          }else{
+            $(this.target).hide( selectedEffect, options, speed, this.effectCallback );
+          }
+          break; 
+        case "effect" :
+          if(selectedEffect == 'none'){
+            $(this.target).show( speed, this.effectCallback );
+          }else{
+            $(this.target).effect( selectedEffect, options, speed, this.effectCallback );
+          }
+          break;
+      }
+      this.isRunnable = false;
+    }
+  },
+
+  effectCallback: function(){
+    this.isRunnable = true;
+  },
+
+  resetEffect: function(){
+    //console.log("reset effect");
+    //$(this.target).stop();
+    var option = this.item.get(this.key);
+    if(option != null){
+      var options = option.split(',');
+      switch(options[0]){
+        case "none" :
+          $(this.target).show();
+          break; 
+        case "show" :
+          $(this.target).hide();
+          break; 
+        case "hide" :
+          $(this.target).show();
+          break; 
+        case "effect" :
+          $(this.target).show();
+          break;
+      }
+    }
+  },
+  
+  appendEffectSelecterTo: function(target){    
+    var selecterTemplate =  $("#effect_selecter_template").html();
+    this.selecter = $(selecterTemplate);
+    $(this.selecter).appendTo(target);
+    
+    var option = this.item.get(this.key);
+    if(option != null){
+      var options = option.split(',');
+      $('.functionTypes option[value="'+options[0]+'"]',this.selecter).prop('selected',true); 
+      $('.effectTypes option[value="'+options[1]+'"]',this.selecter).prop('selected',true); 
+      $('.durations',this.selecter).val(options[2]); 
+    }
+    _self = this;
+    $('.functionTypes',this.selecter).change( _self.setEffect );
+    $('.effectTypes',this.selecter).change( _self.setEffect );
+    //$(this.selecter).find('.easeTypes').change( _self.setEffect );
+    $('.durations',this.selecter).change( _self.setEffect );  
+  },
+
+  setEffect: function(){
+    var optionString = 
+        $(this.selecter).find('.functionTypes').val() + "," +
+        $(this.selecter).find('.effectTypes').val() + "," +
+        //$(this.selecter).find('.easeTypes').val() + "," +
+        $(this.selecter).find('.durations').val();
+    console.log(optionString);
+    this.item.set(this.key,optionString);
+    this.item.save();
+  }
+	
+}
