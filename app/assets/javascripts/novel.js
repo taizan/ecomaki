160c08e50a8853cc50905fb8e94acab82a54cf15
Taizan <taizan@example.com>
2012-11-01 00:28:59 +0900

    hide toolbox at preview

--- app/assets/javascripts/novel.js
+++ app/assets/javascripts/novel.js
@@ -37,17 +37,24 @@ $(function() {
   novelView.appendTo($('#content'));
 
   if(isEditable){
+    $('#toobox').show();
     $('#preview').show().width(100).height(60);
     $('#preview').click(function(){
         isEditable = isEditable ? false : true;
         $('#content').empty();
         novelView = new NovelView({model: novel , isEditable: isEditable});
         novelView.appendTo($('#content'));
-        if(isEditable) $('#preview').html('Preview');
-        else $('#preview').html('Edit');
+        if(isEditable) {
+          $('#preview').html('Preview');
+          $('#toolbox').show();
+        }else{
+          $('#preview').html('Edit');
+          $('#toolbox').hide();
+        }
       });
   
   }
+
 
   $('#console').hide();  
   
