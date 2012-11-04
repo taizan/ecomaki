d55fc94e5fd08cea99355b4e7cc40767c1d38c28
Taizan <taizan@example.com>
2012-11-05 01:12:52 +0900

    fix bug of updating but toolbox view was crushed, so temply using native jquery ui style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require underscore
 //= require config
 //= require bootstrap.min
-//= require jquery-ui-1.8.16.custom.min
+//= require jquery-ui-1.9.1.custom.min
 //= require jquery.iframe-transport
 //= require jquery.fileupload
 //= require jquery.autosize
