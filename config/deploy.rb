1bfda013f2a516e2e8c63f6883b837503378cc1d
yut <>
2012-12-01 17:19:53 +0900

    Implement automatical deployment for database.yml. To use this feature, put database.yml to shared/config/ after cap deploy:setup.

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,3 +30,12 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+
+# Automatical deployment of database.yml
+after 'deploy:update_code', 'deploy:symlink_db'
+namespace :deploy do
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+end
