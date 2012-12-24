aaac7c9b0416d5ffb0ee9711c447f3fa80721718
Taizan <taizan@example.com>
2012-12-24 13:52:28 +0900

    rm console log

--- app/assets/javascripts/novelEdit.js
+++ app/assets/javascripts/novelEdit.js
@@ -51,102 +51,109 @@ $(function() {
   $(window).unload( function () { alert("Bye now!"); } );
   */
                                 
-function initializeView(id,pass,isEditable){
-  _novel = new Novel({id: id,password: pass});
-  _novelView = new NovelView({model: _novel , isEditable: isEditable , isPreview: false});
-  _novelView.appendTo($('#content'));
+  function initializeView(id,pass,isEditable){
+    _novel = new Novel({id: id,password: pass});
+    _novelView = new NovelView({model: _novel , isEditable: isEditable , isPreview: false});
+    _novelView.appendTo($('#content'));
 
-  $('#toolbox').hide();
-  $('#console').hide();  
-  $('#side_menu').hide();
+    $('#toolbox').hide();
+    $('#console').hide();  
+    $('#side_menu').hide();
 
-  $(document).tooltip();
-  $('#static_body').bind('mousedown',onStaticBodyClick);
-}
+    $(document).tooltip();
+    $('#static_body').bind('mousedown',onStaticBodyClick);
+  }
 
-function initializeTool(isEditable){
-  $('#toolbox').show();
+  function initializeTool(isEditable){
+    $('#toolbox').show();
 
-  Picker.prototype.initialize();
+    Picker.prototype.initialize();
 
-  $('#preview_button').click(function(){
-      isEditable = isEditable ? false : true;
-			$('#preview_button img').attr('src', '/assets/novel/' + (isEditable ? 'preview.png' : 'edit.png'));
-			$('#preview_button p').text(isEditable ? 'preview' : 'edit');
-      $('#content').empty();
-      _novelView = new NovelView({model: _novel , isEditable: isEditable, isPreview: true});
-      _novelView.appendTo($('#content'));
-      if(isEditable) {
-        $('#preview p').html('Preview');
-        $('#toolbox').show();
-      }else{
-        $('#preview p').html('Edit');
-        $('#toolbox').hide();
-        $('.tutorial_dialog').hide();
+    $('#preview_button').click(function(){
+        isEditable = isEditable ? false : true;
+			  $('#preview_button img').attr('src', '/assets/novel/' + (isEditable ? 'preview.png' : 'edit.png'));
+			  $('#preview_button p').text(isEditable ? 'preview' : 'edit');
+        $('#content').empty();
+        _novelView = new NovelView({model: _novel , isEditable: isEditable, isPreview: true});
+        _novelView.appendTo($('#content'));
+        if(isEditable) {
+          $('#preview p').html('Preview');
+          $('#toolbox').show();
+        }else{
+          $('#preview p').html('Edit');
+          $('#toolbox').hide();
+          $('.tutorial_dialog').hide();
+        }
+      });
+    
+    // Publish button Click
+    $('#publish_button').click(function(){ 
+        _novel.save({'status': 'publish'}); 
+        alert("作品を公開しました！ソーシャルメディアなどで宣伝しましょう！"); 
+      });
+
+      setTutorial();
+  }
+
+  //チュートリアルセットアップ
+  function setTutorial(){
+    var dialog_top = 40;
+    if( config.getScreenSize().x < 1066) dialog_top = 70;
+    console.log( config.getScreenSize().x);
+    var tutorial = $($('#tutorial_template').html())
+      .appendTo('body')
+      .dialog()
+      .parent().css({ 'top': dialog_top, 'left': 0 , 'position': 'fixed' , 'z-index': 99999}).addClass('tutorial_dialog') ;
+  
+    $('.ui-dialog-titlebar-close',tutorial).click(function(){ $('.tutorial_dialog').hide(); });
+
+    $('#tutorial_button').click(function(){ 
+      //tutorial.dialog('enable');
+        $('.tutorial_dialog')
+          .show()
+          .css({ 'top': dialog_top, 'left': 0 , 'position': 'fixed' , 'z-index': 99999});
+      });
+
+    var tutorial_progless = 0;
+
+    // チュートリアルの進む
+    //
+    $('#tutorial_next_button').click( function() {
+      tutorial_progless++;
+      var next = $('#tutorial_template_' + tutorial_progless ).html();
+      if( next) {
+        $('#tutorial_content').html( next )
+        console.log($('#tutorial_template_' + tutorial_progless ).html());
+        $('#tutorial_back_button').show();
+      }
+      else{
+        $('#tutorial_content').html( 'End' );
+        $('#tutorial_next_button').hide();
       }
     });
-  
-  $('#publish_button').click(function(){ 
-      _novel.save({'status': 'publish'}); 
-      alert("作品を公開しました！ソーシャルメディアなどで宣伝しましょう！"); 
+
+    // チュートリアルの戻る
+    $('#tutorial_back_button').click( function() {
+      tutorial_progless--;
+      var prev = $('#tutorial_template_' + tutorial_progless ).html();
+      if( prev) {
+        $('#tutorial_content').html( prev )
+        //console.log($('#tutorial_template_' + tutorial_progless ).html());
+        $('#tutorial_next_button').show();
+      }
+      else{
+        $('#tutorial_content').html( $('#tutorial_template_0' ).html() );
+        $('#tutorial_back_button').hide(); 
+      }
     });
-  
-  //チュートリアルセットアップ
-  var tutorial = $($('#tutorial_template').html())
-    .appendTo('body')
-    .dialog()
-    .parent().css({ 'top': 40, 'left': 0 , 'position': 'fixed' , 'z-index': 99999}).addClass('tutorial_dialog') ;
-  
-  $('.ui-dialog-titlebar-close',tutorial).click(function(){ $('.tutorial_dialog').hide(); });
+    //console.log($('#tutorial_template').html());
+  }
 
-  $('#tutorial_button').click(function(){ 
-      //tutorial.dialog('enable');
-      $('.tutorial_dialog')
-        .show()
-        .css({ 'top': 40, 'left': 0 , 'position': 'fixed' , 'z-index': 99999});
-    });
-
-  var tutorial_progless = 0;
-
-  // チュートリアルの進む
-  //
-  $('#tutorial_next_button').click( function() {
-    tutorial_progless++;
-    var next = $('#tutorial_template_' + tutorial_progless ).html();
-    if( next) {
-      $('#tutorial_content').html( next )
-      console.log($('#tutorial_template_' + tutorial_progless ).html());
-      $('#tutorial_back_button').show();
-    }
-    else{
-      $('#tutorial_content').html( 'End' );
-      $('#tutorial_next_button').hide();
-    }
-  });
-
-  // チュートリアルの戻る
-  $('#tutorial_back_button').click( function() {
-    tutorial_progless--;
-    var prev = $('#tutorial_template_' + tutorial_progless ).html();
-    if( prev) {
-      $('#tutorial_content').html( prev )
-      console.log($('#tutorial_template_' + tutorial_progless ).html());
-      $('#tutorial_next_button').show();
-    }
-    else{
-      $('#tutorial_content').html( $('#tutorial_template_0' ).html() );
-      $('#tutorial_back_button').hide(); 
-    }
-  });
-
-  //console.log($('#tutorial_template').html());
-}
-
-function onStaticBodyClick(ev){
-  //console.log(ev);
-  TextEditMenu.prototype.onBlur(ev);
-  TextEdit.prototype.onBlur(ev);
-  Picker.prototype.onBlur(ev);
-}
+  function onStaticBodyClick(ev){
+    //console.log(ev);
+    TextEditMenu.prototype.onBlur(ev);
+    TextEdit.prototype.onBlur(ev);
+    Picker.prototype.onBlur(ev);
+  }
 
 });
