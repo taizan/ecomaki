a995c2ad3999e14d6e6a159156422ccf44312b6a
akishika <akishika@ecomaki.com>
2013-02-26 00:52:08 +0900

    refactor function for setting page initialization template

--- app/assets/javascripts/views/pageTemplates.js
+++ app/assets/javascripts/views/pageTemplates.js
@@ -1,42 +1,50 @@
 var selectTemplate = (function(){
 
-	// set templates
-	var setPageByTemplate = {
-		"empty": function(){
-			this.model.chapters.create_after({},-1);
+	var PageTemplate = {};
+
+	PageTemplate["empty"] = {
+		"set": function(novelView){
+			novelView.model.chapters.create_after({},-1);
 		},
+
+		"button": $('<label class="tempalte" id="empty">Empty</label>').button()
+	};
 	
-		"4-cell": function(){
-			var novelView = this;
+	PageTemplate["4-cell"] = {
+		"set": function(novelView){
 			var makeEntry = function(){
 				var chapterView = novelView.childViews[0];
 				//console.log(chapterView);
 				for(var i = 0; i < 4; i++){
+					//接続が重いといくつかはリロードした時に消えるかも
 					chapterView.addEntryWith2Character();
 				}
 			};
-			this.model.chapters.create_after({},-1, {'success': makeEntry});
-		}
+			novelView.model.chapters.create_after({},-1, {'success': makeEntry});
+		},
+
+		"button": $('<label class="template" id="empty">4コマ</label>').button()
 	};
 
-	// you should set UI using this dvi object
-	var buttons = $('<dvi class="initialization" id="select_template"></dvi>');
+	var buttons = $('<dvi class="initialization" id="select_template"></dvi>').css('margin', 'auto');
 	
-	var setPage = function(novelView, temp){
-		// for setting page and removing UI for select template
+	/*
+	var setPage = function(novelView, template){
 		buttons.remove();
-		setPageByTemplate[temp].apply(novelView);
+		setPageByTemplate[template].set(novelView);
 	};
+	*/
 	
 	return function(novelView){
 		// set UI
-		buttons.css('margin', 'auto')
-			.append($('<label>Empty</label>')
-				.button()
-				.click(function(){ setPage(novelView, "empty"); }))
-			.append($('<label>4コマ</label>')
-				.button()
-				.click(function(){ setPage(novelView, "4-cell"); }))
-			.appendTo('body');
+		buttons.appendTo('body');
+
+		$.each(PageTemplate, function(key, obj){
+			buttons.append( obj.button.click( function(){
+				//setPage(novelView, key);
+				buttons.remove();
+				PageTemplate[key].set(novelView);
+			} ) );
+		})
 	};
 })();
