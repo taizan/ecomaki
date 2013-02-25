f3ed444f0315dcb1efee9a74378668a18b0de206
akishika <akishika@ecomaki.com>
2013-02-26 01:21:43 +0900

    edit the design of initialization

--- app/assets/javascripts/views/pageTemplates.js
+++ app/assets/javascripts/views/pageTemplates.js
@@ -7,7 +7,17 @@ var selectTemplate = (function(){
 			novelView.model.chapters.create_after({},-1);
 		},
 
-		"button": $('<label class="tempalte" id="empty">Empty</label>').button()
+		"button": $('<label class="tempalte" id="empty">Free<br/>Style</label>')
+			.css({
+				'position': 'absolute',
+				'left': $(window).width()/2-180,
+				'top': $(window).height()/2-60,
+				'width': '120px',
+				'height': '120px',
+				'font-size': '20pt',
+				'background': '#FFFFFF'
+			})
+			.button()
 	};
 	
 	PageTemplate["4-cell"] = {
@@ -23,10 +33,30 @@ var selectTemplate = (function(){
 			novelView.model.chapters.create_after({},-1, {'success': makeEntry});
 		},
 
-		"button": $('<label class="template" id="empty">4コマ</label>').button()
+		"button": $('<label class="template" id="empty">4コマ</label>')
+			.css({
+				'position': 'absolute',
+				'left': $(window).width()/2+30,
+				'top': $(window).height()/2-60,
+				'width': '120px',
+				'height': '120px',
+				'font-size': '20pt',
+				'background': '#FFFFFF'
+			})
+			.button()
 	};
 
-	var buttons = $('<dvi class="initialization" id="select_template"></dvi>').css('margin', 'auto');
+	var buttons = $('<dvi class="initialization" id="select_template"></dvi>')
+		.css({
+			'position': 'fixed',
+			'left': 0,
+			'top': 0,
+			'width': $(window).width(),
+			'height': $(window).height(),
+			'z-index': 100000,
+			'background': 'rgba(0, 0, 0, 0.7)',
+			'margin': 'auto'
+		});
 	
 	/*
 	var setPage = function(novelView, template){
