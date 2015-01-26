a41f70b9f6037ba410bf862d82dad0faa4a9cea4
Taizan <taizan@example.com>
2015-01-26 16:42:28 +0900

    fix miss of type index

--- app/assets/javascripts/models.js
+++ app/assets/javascripts/models.js
@@ -379,8 +379,8 @@ var Chapter = Backbone.Model.extend({
   },
 
   create_entry_from_template: function(){
-    var type = ( this.entries.length -1) % 4;
-    this.entries.create_after( EntryTemplate.prototype.getTemplate(type) ,  this.entries.length-1 );
+    var type = ( this.entries.length ) % 4;
+    this.entries.create_after( EntryTemplate.prototype.getTemplate(type) ,  this.entries.length );
   },
 
 });
